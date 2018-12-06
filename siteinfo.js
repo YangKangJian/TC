a12f01a40875e7a48a1f6b2b1d93e57ba22615f2
YangKangJian <2567383143@qq.com>
2018-12-06 17:15:48 +0800

    init

---
+++ siteinfo.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: "Demo",
+    uniacid: "2",
+    acid: "2",
+    multiid: "0",
+    version: "1.0",
+    siteroot: "https://www.haokeyu.com/index.php",
+    design_method: "3"
+};
+//小程序的id获取办法为：1）打开微擎后台-点击小程序菜单-选择对应的小程序
+                      //2）点击右上角的管理按钮
+                      //3）点击基础信息，拉到最下方有个消息推送配置
+                      //4）URL里面的id=xx，这个数字就是小程序的id
